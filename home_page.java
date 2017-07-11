37faec941186577858993a8329a09cd311edf78f
it'sTimeToPPPPPProgram <shui_onn@hotmail.com>
2017-07-11 11:07:07 +0800

    Add files via upload

---
+++ home_page.java
@@ -0,0 +1,17 @@
+package com.example.securitility2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shui onn on 10/7/2017.
+ */
+
+public class home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
